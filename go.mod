5c2a4faa75555407eeaf1f903f88d16c8b882249
Jean-Léon Henry <92361407+JeanLeonHenry@users.noreply.github.com>
2024-08-10 15:26:11 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JeanLeonHenry/chirpy
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{Addr: "localhost:8080", Handler: mux}
+	server.ListenAndServe()
+}
