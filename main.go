d2f4b07dbd141ff9e563cac2b817da0811b98c61
Jean-Léon Henry <92361407+JeanLeonHenry@users.noreply.github.com>
2024-08-22 09:02:57 +0200

    content & handlers

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// BUG: only one increment
+	cfg.fileserverHits++
+	log.Print(cfg)
+	return next
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprint("Hits: ", cfg.fileserverHits)))
+}
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
 func main() {
 	mux := http.NewServeMux()
-	server := http.Server{Addr: "localhost:8080", Handler: mux}
-	server.ListenAndServe()
+	server := http.Server{Addr: ":8080", Handler: mux}
+	cfg := apiConfig{fileserverHits: 0}
+	rootServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app", rootServer)))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
